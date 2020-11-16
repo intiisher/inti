a86d1e65e6bf1fb538f17b0716fa43c3a05b8553
intiisher <67817360+intiisher@users.noreply.github.com>
2020-11-16 12:09:20 +0600

    add

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Mohd Arafat Intisher</title>
+  <meta name="description" content="Web Apps Developer for ORBL by day. Student of Digital Entrepreneurship at EUV by night. Amateur athlete throughout the week.">
+
+  <link rel="stylesheet" href="https://drive.google.com/file/d/1WfQZu0l47szqAMtBeHiWBVK5E1Wa7TIi/view?usp=sharing">
+
+  
+  <link href="https://fonts.googleapis.com/css?family=Raleway+Dots" rel="stylesheet" type="text/css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  </head>
+
+
+  <body>
+    <div class='home'>
+
+  <div class='dot-tone-portrait'>
+    <img src='face-bw.JPG'/>
+  </div>
+
+<div class='home-content'>
+    <h1>Mohd Arafat Intisher</h1>
+    <span class='label'>Web Developer, IT System Analyst, amateur athlete</span>
+
+    <span class='bigspan'>Currently at <br/> Orogenic Resources— <br/>
+       ➀&nbsp;working on Web Application development,<br/>
+        ➁&nbsp;designing IT systems and platforms, and <br/>
+          ➂&nbsp;administering Servers and Databases.</span>
+
+    <p><a href='/work' class='bigLinkJob' style="color: #00A522;"><u> - Web Apps Developer for ORBL by day.</u></a></p>
+    <p><a href='/work' class='bigLinkUni' style="color: #00A522;"><u> - Student of Digital Entrepreneurship at EUV by night.</u></a></p>
+    <p><a href='/work' class='bigLinkAth' style="color: #00A522;"><u> - Amateur athlete throughout the week.</u></a></p>
+
+    <!-- <h2>Talks</h2>
+
+    <table class='elsewhere-list'>
+      <tbody>
+        <tr>
+          <td>Accelerate Good Global</td>
+          <td>San Francisco</td>
+        </tr>
+        <tr>
+          <td>Workshop on Twitter bots</td>
+          <td>The New School, New York</td>
+        </tr>
+        <tr>
+          <td>Typographische Gesellschaft</td>
+          <td>München</td>
+        </tr>
+        <tr>
+          <td>Panel at SRCCON</td>
+          <td>Philadelphia</td>
+        </tr>
+        <tr>
+          <td>Books and Browsers</td>
+          <td>San Francisco</td>
+        </tr>
+        <tr>
+          <td>Future of Web Design</td>
+          <td>New York</td>
+        </tr>
+        <tr>
+          <td>Panel at Tools of Change</td>
+          <td>New York</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h2>Published</h2>
+
+    <table class='elsewhere-list'>
+      <tbody>
+        <tr>
+          <td>A List Apart</td>
+          <td><a href='https://alistapart.com/article/gardens-not-graves'>Gardens, Not Graves</a></td>
+        </tr>
+        <tr>
+          <td>Interview with Source</td>
+          <td><a href='https://source.opennews.org/articles/behind-scenes-nyt-redesign/'>Behind the Scenes on the NYT Redesign</a></td>
+        </tr>
+        <tr>
+          <td>Stet</td>
+          <td><a href='https://stet.editorially.com/articles/attention-rhythm-and-weight/'>Attention, Rhythm &amp; Weight</a></td>
+        </tr>
+        <tr>
+          <td>Contents Magazine</td>
+          <td><a href='https://contentsmagazine.com/articles/made-to-measure/'>Made to Measure</a></td>
+        </tr>
+      </tbody>
+    </table> -->
+
+    <div class="row" style="background-color:  #00A522; text-align: center;" >
+      <div class="col-sm-6 col-sm-offset-3">
+          <div class="well" style="margin-top: 00%;">
+          <h3 style="color:whitesmoke; "> <b><u>Send me a message</u></b></h3>
+          <form action="mailto:you@yourdmainhere.com" method="post" enctype="text/plain" role="form" id="contactForm" data-toggle="validator" class="shake">
+              <div class="row">
+                  <div class="form-group col-sm-5">
+                      <!-- <label for="name" class="h4">Name</label> -->
+                      <input style=" text-align: center;" type="text" class="form-control" id="name" placeholder="Enter name" required data-error="NEW ERROR MESSAGE">
+                      <div class="help-block with-errors"></div>
+                  </div>
+
+                  <div class="form-group col-sm-5">
+                      <!-- <label for="email" class="h4">Email</label> -->
+                      <input style=" text-align: center;"  type="email" class="form-control" id="email" rows="5" placeholder="Enter email" required>
+                      <div class="help-block with-errors"></div>
+                  </div>
+
+                </div>
+
+                  <div class= "form-group col-sm-6">
+                  <!-- <label for="message" class="h4 ">Message</label> -->
+                    <textarea style=" text-align: center;"  id="message" class="form-control" rows="5" placeholder="Enter your message" required></textarea>
+                    <div class="help-block with-errors"></div>
+                  </div>
+                    <button type="submit" id="form-submit" class="btn btn-success btn-lg  ">Submit</button>
+                    
+                  <div id="msgSubmit" class="h3 text-center hidden"></div>
+                  <div class="clearfix"></div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="row " style="background-color:  #00A522; " >
+
+          <div class="well" style="margin-top: .5%;">
+          <div style="text-align: center;">
+          <div style="color:whitesmoke; "> <b><u>Other ways to contact me</u></b></div>
+          <div style="text-align: center;">
+            <section style="text-align: center;" >
+            <ul id="services">
+            <table >
+            <tr> 
+            <th style="text-align: center;"> 
+              <i class="fa fa-phone" aria-hidden="true" ></i>
+            </th>
+            <th>
+              <ul style="color: #00A522; " ><strong>+ 8801710479715</strong></ul> 
+            </th>
+            
+            <li>
+              <div class="phone" >
+                  <i class="fa fa-phone" aria-hidden="true" ></i>
+              </div>
+              <!-- <span>Facebook</span> -->
+            </li>
+           </th>
+            <td>
+            <li>
+              <ul style="color:whitesmoke; " ><strong>+8801710479715</strong></ul> 
+            </li>
+           
+          </td>
+          </tr>
+          </table>
+          </section >
+            </div>
+              <div class="row smicons">
+              <section >
+                <ul id="services">
+                  
+                  
+                  <li>
+                    <div class="facebook" >
+                      <a href="https://facebook.com/colorlib/">
+                        <i class="fa fa-facebook " aria-hidden="true" ></i>
+                      </a>
+                    </div>
+                    <!-- <span>Facebook</span> -->
+                  </li>
+                  <li>
+                    <div class="twitter">
+                      <a href="https://twitter.com/colorlib/">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                      </a>
+                    </div>
+                    <!-- <span>Twitter</span> -->
+                  </li>
+                  <li>
+                    <div class="youtube">
+                      <a href="https://www.youtube.com/c/Colorlib">
+                        <i class="fa fa-youtube" aria-hidden="true"></i>
+                      </a>
+                    </div>
+                    <!-- <span>YouTube</span> -->
+                  </li>
+                  <li>
+                    <div class="linkedin">
+                      <a href="https://www.linkedin.com/company/colorlib">
+                        <i class="fa fa-linkedin" aria-hidden="true"></i>
+                      </a>
+                    </div>
+                    <!-- <span>LinkedIn</span> -->
+                  </li>
+                  <li>
+                    <div class="instagram">
+                      <a href="https://www.linkedin.com/company/colorlib">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                      </a>
+                    </div>
+                    <!-- <span>Instagram</span> -->
+                  </li>
+                  <li>
+                    <div class="github">
+                      <a href="https://github.com/puikinsh/">
+                        <i class="fa fa-github" aria-hidden="true"></i>
+                      </a>
+                    </div>
+                    <!-- <span>Github</span> -->
+                  </li>
+                </ul>
+              
+              
+      </div>
+  <div>
+    </div>
+
+
+  </div>
+</div>
+
+
+
+
+
+  <script type='text/javascript'>
+    if (document.documentElement.clientWidth > 600) {
+      document.body.appendChild(document.createElement("script")).src = '/breathing-halftone.js';
+ document.body.appendChild(document.createElement("script")).src = '/face.js';
+ document.body.appendChild(document.createElement("script")).src = '/jquery-1.11.2.min.js';
+ document.body.appendChild(document.createElement("script")).src = '/validator.min.js';
+ document.body.appendChild(document.createElement("script")).src = '/form-scripts.js';
+    }
+  </script>
+
+</div>
+
+  </body>
+
+</html>
